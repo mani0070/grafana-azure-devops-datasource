b1ab6f4c932101f3268bdecfbda71d647933b0c8
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-08-05 09:35:12 +0100

    DataSourceHttpSettings removed in config editor

--- src/editors/AzureDevopsConfigEditor.tsx
+++ src/editors/AzureDevopsConfigEditor.tsx
@@ -1,17 +1,89 @@
-import React, { PureComponent } from 'react';
-import { DataSourceHttpSettings } from '@grafana/ui';
+import React, { PureComponent, ChangeEvent } from 'react';
 import { DataSourcePluginOptionsEditorProps, DataSourceJsonData } from '@grafana/data';
 
-interface AzureDevopsOptions extends DataSourceJsonData {}
+interface AzureDevopsOptions extends DataSourceJsonData {
+  url?: string;
+}
+interface AzureDevopsDatasourceSecureJsonData {
+  basicAuthPassword?: string;
+}
+type Props = DataSourcePluginOptionsEditorProps<AzureDevopsOptions>;
+interface State {}
 
-export type Props = DataSourcePluginOptionsEditorProps<AzureDevopsOptions>;
-
-export class AzureDevopsConfigEditor extends PureComponent<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
+export class AzureDevopsConfigEditor extends PureComponent<Props, State> {
+  onAzureDevopsURLChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({ ...options, url: event.target.value });
+  };
+  onPATTokenChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonData: {
+        basicAuthPassword: event.target.value,
+      },
+    });
+  };
+  onResetPATToken = () => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        basicAuthPassword: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        basicAuthPassword: '',
+      },
+    });
+  };
   render() {
-    const { options, onOptionsChange } = this.props;
-    return <DataSourceHttpSettings defaultUrl="https://dev.azure.com/<INSTANCE>" dataSourceConfig={options} onChange={onOptionsChange} />;
+    const { options } = this.props;
+    options.basicAuth = true;
+    options.basicAuthUser = 'username';
+    const { secureJsonFields } = options;
+    const secureJsonData = (options.secureJsonData || {}) as AzureDevopsDatasourceSecureJsonData;
+    return (
+      <>
+        <h3 className="page-heading">Azure Devops Settings</h3>
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <label className="gf-form-label width-11" title="Azure Devops URL">
+              Azure Devops URL
+            </label>
+            <input
+              className="gf-form-input width-20"
+              type="text"
+              onChange={this.onAzureDevopsURLChange}
+              value={options.url || ''}
+              placeholder="https://dev.azure.com/YOUR_INSTANCE_NAME"
+            ></input>
+          </div>
+        </div>
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <label className="gf-form-label width-11" title="API Key">
+              PAT Token
+            </label>
+            {((secureJsonFields && secureJsonFields.basicAuthPassword) as boolean) ? (
+              <>
+                <label className="gf-form-label width-20">Configured</label>
+                <span className="gf-form-button btn btn-secondary width-6" onClick={this.onResetPATToken}>
+                  Reset
+                </span>
+              </>
+            ) : (
+              <input
+                type="password"
+                value={secureJsonData.basicAuthPassword || ''}
+                className="gf-form-input width-20"
+                onChange={this.onPATTokenChange}
+              ></input>
+            )}
+          </div>
+        </div>
+      </>
+    );
   }
 }
